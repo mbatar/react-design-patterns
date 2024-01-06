e6cc372562acd50902de50fa8264bd242ba13353
MFFED <mffed@Mustafas-MBP-2.home>
2024-01-06 19:45:55 +0300

    converted component to arrow function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import HookExample from "./hook";
 import ProviderExample from "./provider";
 import HOCExample from "./higher-order-component";
 import PrestationalAndContainerExample from "./presentational-and-container-component";
-function App() {
+
+const App = () => {
   return (
     <div>
       <HOCExample />
+      <HookExample />
       <ProviderExample />
       <PrestationalAndContainerExample />
     </div>
   );
-}
+};
 
 export default App;
