0f0cc80094985fe52e32d35c5027495bcd7116de
MFFED <mffed@Mustafas-MBP-2.home>
2024-01-06 19:55:52 +0300

    created render-prop example

---
+++ src/render-prop/index.js
@@ -0,0 +1,12 @@
+import Input from "./Input";
+
+const RenderPropBase = () => {
+  return (
+    <Input
+      renderKelvin={({ value }) => <div className="temp">{value}K</div>}
+      renderFahrenheit={({ value }) => <div className="temp">{value}°F</div>}
+    />
+  );
+};
+
+export default RenderPropBase;
